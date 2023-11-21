889826450b41415d3eb5d319a24ed05d246308c9
rmshimomura <shimomurarodrigo1@gmail.com>
2023-11-21 15:54:45 -0300

    .

--- whiteboard/tests/tcp_test.py
+++ whiteboard/tests/tcp_test.py
@@ -8,20 +8,22 @@ s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s2.connect(('127.0.0.1', 1000))
 
 
-first = True
+
 
 
 conn1, addr1 = s1.accept()
-print('conn1: ', addr1)
-if first:
-    s2.send('hello'.encode())
+print('conn1: ', conn1)
+print(s2)
+
+s2.send('hello'.encode())
 print(conn1.recv(1024))
+
 conn1.send('hello back'.encode())
 print(s2.recv(1024))
+
 conn1.close()   
 
 s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s3.connect(('127.0.0.1', 1000))
 conn, (ip, port) = s1.accept()
-print('conn2: ', ip, port)
-    
+print('conn2: ', conn.getpeername())
